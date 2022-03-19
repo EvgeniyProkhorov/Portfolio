027025025adb7f1894166164a558ac01792fe34c
EvgeniyProkhorov <firerequest@gmail.com>
2022-03-19 15:07:38 +0300

    code upd

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
         <div className={css.footer}>
             <div className={`${styleContainer.container} ${css.container}`}>
                 <h2>Evgeniy Prokhorov</h2>
-                <div className={css.socialMedia}>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
+                <div className={css.socialIcons}>
+                    <div className={css.socialIcon}>1</div>
+                    <div className={css.socialIcon}>2</div>
+                    <div className={css.socialIcon}>3</div>
+                    <div className={css.socialIcon}>4</div>
                 </div>
                 <span> 2022 copyright</span>
             </div>
