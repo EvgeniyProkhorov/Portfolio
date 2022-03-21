aacb85d6a54b829f0bdec6d5b892620a6b812829
EvgeniyProkhorov <firerequest@gmail.com>
2022-03-21 18:03:55 +0300

    scss refactor

--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -1,12 +1,12 @@
 import React from "react";
-import css from './Skill.module.css'
+import css from './Skill.module.scss'
 
 
 const Skill = (props) => {
     return (
         <div className={css.skill}>
             <div className={css.icon}></div>
-            <h3>{props.title}</h3>
+            <h3 className={css.skillTitle}>{props.title}</h3>
             <span className={css.description}>{props.description}</span>
         </div>
     )
