a2e69f2448d512a27c092a8b20db6eaf3ffc991e
EvgeniyProkhorov <firerequest@gmail.com>
2022-04-23 23:44:56 +0300

    scss refactor

--- src/main/Main.js
+++ src/main/Main.js
@@ -24,7 +24,7 @@ const Main = () => {
                        loaded={particlesLoaded}
                        options={{
                            style: {
-                               position: "absolute",
+                               // position: "absolute",
                                top: 0,
                                left: 0,
                                bottom: 0,
