b8ed612f0fe806e099e68182a0e57b76d771444b
EvgeniyProkhorov <firerequest@gmail.com>
2022-04-24 22:12:12 +0300

    scss refactor

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
 import css from './Footer.module.scss'
+import linkedinIcon from '../assets/contactsIcons/linkedin500.png'
+import telegramIcon from '../assets/contactsIcons/telegram500.png'
+import gitHubIcon from '../assets/contactsIcons/github500.png'
+
+const telegram = 'https://t.me/EvgenFrontEnd'
+const gitHub = 'https://github.com/EvgeniyProkhorov'
 
 
 const Footer = () => {
@@ -8,10 +14,9 @@ const Footer = () => {
             <div className={css.container}>
                 <h2 className={css.title}>Evgeniy Prokhorov</h2>
                 <div className={css.socialIcons}>
-                    <a href={''} className={css.socialIcon}></a>
-                    <div className={css.socialIcon}></div>
-                    <div className={css.socialIcon}></div>
-                    <div className={css.socialIcon}></div>
+                    <a target={'_blank'} href={''}><img className={css.socialIcon} src={linkedinIcon} alt={'LinkedIn'}/></a>
+                    <a target={'_blank'} href={telegram}><img className={css.socialIcon} src={telegramIcon} alt={'Telegram'}/></a>
+                    <a target={'_blank'} href={gitHub}><img className={css.socialIcon} src={gitHubIcon} alt={'GitHub'}/></a>
                 </div>
                 <span className={css.title}> 2022 copyright</span>
             </div>
